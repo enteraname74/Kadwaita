904a7ccfd55fa8ae7387cabe5de5370f3861283b
enteraname74 <noahpenin@gmail.com>
2024-05-03 19:34:35 +0200

    Add lib module

---
+++ kadwaitalib/build.gradle.kts
@@ -0,0 +1,46 @@
+plugins {
+    id("org.jetbrains.kotlin.multiplatform")
+    id("com.android.library")
+    id("org.jetbrains.compose")
+}
+
+group = "com.github.enteraname74.kadwaita"
+version = "0.1.0"
+
+kotlin {
+    androidTarget()
+    jvm("desktop")
+    jvmToolchain(17)
+
+    sourceSets {
+        commonMain {
+            dependencies {
+                implementation(compose.ui)
+                implementation(compose.components.resources)
+            }
+        }
+    }
+}
+
+android {
+    namespace = "com.github.enteraname74.kadwaita"
+    compileSdk = libs.versions.android.compileSdk.get().toInt()
+
+    defaultConfig {
+        minSdk = libs.versions.android.minSdk.get().toInt()
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlin {
+        jvmToolchain(17)
+    }
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
